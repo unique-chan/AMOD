562d516ceaecda3f66aa21fe722ac20d84bdfbf6
unique-chan <yechankim@gm.gist.ac.kr>
2025-02-02 23:02:46 +0900

    config modified to run

--- my_config/oriented_rcnn_swinS_fpn_1x_amod_le90.py
+++ my_config/oriented_rcnn_swinS_fpn_1x_amod_le90.py
@@ -1,93 +1,11 @@
+# 🚩 DEFAULT CONFIG ####################################################################################################
 dataset_type = 'AMODDataset'
 angles = [0,]
-data_root = 'data/AMOD_V1/' # Should be changed properly!
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
-    train=dict(
-        type=dataset_type,
-        data_root=data_root,
-        ann_file='train.txt',
-        img_prefix='train',
-        angles=angles,
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(type='PolyRandomRotate'),
-            dict(type='RRandomCrop', crop_size=(1000, 1000)),
-            dict(type='RResize',
-                 img_scale=[(800, 800), (2000, 2000)],  # 0.8x ~ 2x
-                 multiscale_mode='range'),
-            dict(
-                type='RRandomFlip',
-                flip_ratio=[0.25, 0.25, 0.25],
-                direction=['horizontal', 'vertical', 'diagonal'],
-                version='le90'),
-            dict(
-                type='Normalize',
-                mean=img_norm_cfg['mean'],
-                std=img_norm_cfg['std'],
-                to_rgb=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='DefaultFormatBundle'),
-            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-        ],
-        version='le90'),
-    val=dict(
-        type=dataset_type,
-        data_root=data_root,
-        ann_file='val.txt',
-        img_prefix='train',
-        angles=angles,
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(1920, 1440),
-                flip=False,
-                transforms=[
-                    dict(type='RResize'),
-                    dict(
-                        type='Normalize',
-                        mean=img_norm_cfg['mean'],
-                        std=img_norm_cfg['std'],
-                        to_rgb=True),
-                    dict(type='Pad', size_divisor=32),
-                    dict(type='DefaultFormatBundle'),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ],
-        version='le90'),
-    test=dict(
-        type=dataset_type,
-        data_root=data_root,
-        ann_file='test.txt',
-        img_prefix='test',
-        angles=angles,
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(1920, 1440),
-                flip=False,
-                transforms=[
-                    dict(type='RResize'),
-                    dict(
-                        type='Normalize',
-                        mean=img_norm_cfg['mean'],
-                        std=img_norm_cfg['std'],
-                        to_rgb=True),
-                    dict(type='Pad', size_divisor=32),
-                    dict(type='DefaultFormatBundle'),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ],
-        version='le90')
-)
+data_root = 'data/AMOD_V1/'
+load_from = None
+resume_from = None
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+angle_version = 'le90'
 evaluation = dict(interval=1, metric='mAP', save_best='mAP')
 optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
@@ -96,25 +14,64 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.3333333333333333,
-    step=[16, 22])
+    step=[16, 22]
+)
 runner = dict(type='EpochBasedRunner', max_epochs=30)
 checkpoint_config = dict(interval=-1) # save only when val mAP is best
 log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook'),
                                       dict(type='TensorboardLoggerHook')])
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = None
-resume_from = None
 workflow = [('train', 1)]
 opencv_num_threads = 0
 mp_start_method = 'fork'
-angle_version = 'le90'
 
-pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth'
+
+# 🧑‍🏫 TRAIN/VAL/TEST CONFIG #############################################################################################
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RResize',
+        img_scale=[(800, 800), (1600,1600)],
+        multiscale_mode='range'),
+    dict(type='RRandomFlip',
+         flip_ratio=[0.25, 0.25, 0.25],
+         direction=['horizontal', 'vertical', 'diagonal'],
+         version=angle_version),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=[(1200, 1200)],
+        transforms=[
+            dict(type='RResize'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            # dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(type=dataset_type, data_root=data_root, ann_file='train.txt', img_prefix='train', angles=angles,
+               pipeline=train_pipeline, version=angle_version),
+    val=dict(type=dataset_type, data_root=data_root, ann_file='val.txt', img_prefix='train', angles=angles,
+             pipeline=test_pipeline, version=angle_version),
+    test=dict(type=dataset_type, data_root=data_root, ann_file='test.txt', img_prefix='test', angles=angles,
+              pipeline=test_pipeline, version=angle_version)
+)
+
+
+# 🤖 MODEL CONFIG ######################################################################################################
 model = dict(
     type='OrientedRCNN',
     backbone=dict(
-        # _delete_=True,
         type='SwinTransformer',
         embed_dims=96,
         depths=[2, 2, 18, 2],
@@ -130,33 +87,38 @@ model = dict(
         out_indices=(0, 1, 2, 3),
         with_cp=False,
         convert_weights=True,
-        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+        init_cfg=dict(type='Pretrained', checkpoint='https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth')
+    ),
     neck=dict(
         type='FPN',
         in_channels=[96, 192, 384, 768],
         out_channels=256,
-        add_extra_convs='on_output',
-        num_outs=5,
-        relu_before_extra_convs=True),
+        num_outs=5
+    ),
     rpn_head=dict(
         type='OrientedRPNHead',
         in_channels=256,
         feat_channels=256,
-        version='le90',
+        version=angle_version,
         anchor_generator=dict(
             type='AnchorGenerator',
             scales=[8],
             ratios=[0.5, 1.0, 2.0],
-            strides=[4, 8, 16, 32, 64]),
+            strides=[4, 8, 16, 32, 64]
+        ),
         bbox_coder=dict(
             type='MidpointOffsetCoder',
-            angle_range='le90',
+            angle_range=angle_version,
             target_means=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-            target_stds=[1.0, 1.0, 1.0, 1.0, 0.5, 0.5]),
+            target_stds=[1.0, 1.0, 1.0, 1.0, 0.5, 0.5]
+        ),
         loss_cls=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0
+        ),
         loss_bbox=dict(
-            type='SmoothL1Loss', beta=0.1111111111111111, loss_weight=1.0)),
+            type='SmoothL1Loss', beta=0.1111111111111111, loss_weight=1.0
+        )
+    ),
     roi_head=dict(
         type='OrientedStandardRoIHead',
         bbox_roi_extractor=dict(
@@ -165,27 +127,35 @@ model = dict(
                 type='RoIAlignRotated',
                 out_size=7,
                 sample_num=2,
-                clockwise=True),
+                clockwise=True
+            ),
             out_channels=256,
-            featmap_strides=[4, 8, 16, 32]),
+            featmap_strides=[4, 8, 16, 32]
+        ),
         bbox_head=dict(
             type='RotatedShared2FCBBoxHead',
             in_channels=256,
             fc_out_channels=1024,
             roi_feat_size=7,
-            num_classes=13, # AMOD has thirteen classes!
+            num_classes=15,
             bbox_coder=dict(
                 type='DeltaXYWHAOBBoxCoder',
-                angle_range='le90',
+                angle_range=angle_version,
                 norm_factor=None,
                 edge_swap=True,
                 proj_xy=True,
                 target_means=(0.0, 0.0, 0.0, 0.0, 0.0),
-                target_stds=(0.1, 0.1, 0.2, 0.2, 0.1)),
+                target_stds=(0.1, 0.1, 0.2, 0.2, 0.1)
+            ),
             reg_class_agnostic=True,
             loss_cls=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))),
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0
+            ),
+            loss_bbox=dict(
+                type='SmoothL1Loss', beta=1.0, loss_weight=1.0
+            )
+        )
+    ),
     train_cfg=dict(
         rpn=dict(
             assigner=dict(
@@ -194,21 +164,25 @@ model = dict(
                 neg_iou_thr=0.3,
                 min_pos_iou=0.3,
                 match_low_quality=True,
-                ignore_iof_thr=-1),
+                ignore_iof_thr=-1
+            ),
             sampler=dict(
                 type='RandomSampler',
                 num=256,
                 pos_fraction=0.5,
                 neg_pos_ub=-1,
-                add_gt_as_proposals=False),
+                add_gt_as_proposals=False
+            ),
             allowed_border=0,
             pos_weight=-1,
-            debug=False),
+            debug=False
+        ),
         rpn_proposal=dict(
             nms_pre=2000,
             max_per_img=2000,
             nms=dict(type='nms', iou_threshold=0.8),
-            min_bbox_size=0),
+            min_bbox_size=0
+        ),
         rcnn=dict(
             assigner=dict(
                 type='MaxIoUAssigner',
@@ -217,24 +191,32 @@ model = dict(
                 min_pos_iou=0.5,
                 match_low_quality=False,
                 iou_calculator=dict(type='RBboxOverlaps2D'),
-                ignore_iof_thr=-1),
+                ignore_iof_thr=-1
+            ),
             sampler=dict(
                 type='RRandomSampler',
                 num=512,
                 pos_fraction=0.25,
                 neg_pos_ub=-1,
-                add_gt_as_proposals=True),
+                add_gt_as_proposals=True
+            ),
             pos_weight=-1,
-            debug=False)),
+            debug=False
+        )
+    ),
     test_cfg=dict(
         rpn=dict(
             nms_pre=2000,
             max_per_img=2000,
             nms=dict(type='nms', iou_threshold=0.8),
-            min_bbox_size=0),
+            min_bbox_size=0
+        ),
         rcnn=dict(
             nms_pre=2000,
             min_bbox_size=0,
             score_thr=0.05,
             nms=dict(iou_thr=0.1),
-            max_per_img=2000)))
+            max_per_img=2000
+        )
+    )
+)
