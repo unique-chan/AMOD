2ea7c366942d6beb2ad9e676bcb4387f37de575e
unique-chan <yechankim@gm.gist.ac.kr>
2025-02-07 21:50:44 +0900

    AMOD_finegrained +

--- mmrotate/mmrotate/datasets/__init__.py
+++ mmrotate/mmrotate/datasets/__init__.py
@@ -6,4 +6,5 @@ from .pipelines import *  # noqa: F401, F403
 from .sar import SARDataset  # noqa: F401, F403
 from .amod import *  # noqa: F401, F403
 
-__all__ = ['SARDataset', 'DOTADataset', 'build_dataset', 'HRSCDataset', 'AMODDataset']
+__all__ = ['SARDataset', 'DOTADataset', 'build_dataset', 'HRSCDataset',
+           'AMODDataset', 'AMODFineGrainedDataset']
