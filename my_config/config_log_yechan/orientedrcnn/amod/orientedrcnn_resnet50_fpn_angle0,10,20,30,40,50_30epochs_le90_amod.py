e782ee5fcd73cb11f574873c89230c952ad0c941
unique-chan <yechankim@gm.gist.ac.kr>
2025-03-26 16:34:00 +0900

    +

--- my_config/config_log_yechan/orientedrcnn/amod/orientedrcnn_resnet50_fpn_angle0,10,20,30,40,50_30epochs_le90_amod.py
+++ my_config/config_log_yechan/orientedrcnn/amod/orientedrcnn_resnet50_fpn_angle0,10,20,30,40,50_30epochs_le90_amod.py
@@ -93,7 +93,6 @@ model = dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        start_level=1,
         num_outs=5),
     rpn_head=dict(
         type='OrientedRPNHead',
