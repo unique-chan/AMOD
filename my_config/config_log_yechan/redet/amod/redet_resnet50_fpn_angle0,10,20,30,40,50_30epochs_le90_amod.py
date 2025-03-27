cac55a02d42109bdf8bcc7116ff7f90d06b98aef
unique-chan <yechankim@gm.gist.ac.kr>
2025-03-27 14:37:34 +0900

    +

--- my_config/config_log_yechan/redet/amod/redet_resnet50_fpn_angle0,10,20,30,40,50_30epochs_le90_amod.py
+++ my_config/config_log_yechan/redet/amod/redet_resnet50_fpn_angle0,10,20,30,40,50_30epochs_le90_amod.py
@@ -140,7 +140,6 @@ model = dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        start_level=1,
         add_extra_convs='on_input',
         num_outs=5),
     # backbone=dict(
