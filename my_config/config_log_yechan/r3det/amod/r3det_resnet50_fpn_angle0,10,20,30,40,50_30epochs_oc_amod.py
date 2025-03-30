9e92868ce5f8a9f2f3d7fa49e73975addac19401
unique-chan <yechankim@gm.gist.ac.kr>
2025-03-30 18:17:25 +0900

    +

--- my_config/config_log_yechan/r3det/amod/r3det_resnet50_fpn_angle0,10,20,30,40,50_30epochs_oc_amod.py
+++ my_config/config_log_yechan/r3det/amod/r3det_resnet50_fpn_angle0,10,20,30,40,50_30epochs_oc_amod.py
@@ -229,23 +229,23 @@ model = dict(
         nms=dict(iou_thr=0.1),
         max_per_img=2000))
 
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RResize', img_scale=(1024, 1024)),
-    dict(
-        type='RRandomFlip',
-        flip_ratio=[0.25, 0.25, 0.25],
-        direction=['horizontal', 'vertical', 'diagonal'],
-        version=angle_version),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-]
-data = dict(
-    train=dict(pipeline=train_pipeline, version=angle_version),
-    val=dict(version=angle_version),
-    test=dict(version=angle_version))
+# img_norm_cfg = dict(
+#     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# train_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(type='LoadAnnotations', with_bbox=True),
+#     dict(type='RResize', img_scale=(1024, 1024)),
+#     dict(
+#         type='RRandomFlip',
+#         flip_ratio=[0.25, 0.25, 0.25],
+#         direction=['horizontal', 'vertical', 'diagonal'],
+#         version=angle_version),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='Pad', size_divisor=32),
+#     dict(type='DefaultFormatBundle'),
+#     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+# ]
+# data = dict(
+#     train=dict(pipeline=train_pipeline, version=angle_version),
+#     val=dict(version=angle_version),
+#     test=dict(version=angle_version))
