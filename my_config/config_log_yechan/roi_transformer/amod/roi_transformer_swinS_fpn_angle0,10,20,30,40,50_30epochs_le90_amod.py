a6a8c1b6d48cee09e14d8b71541c813e61023d17
unique-chan <yechankim@gm.gist.ac.kr>
2025-03-31 11:02:09 +0900

    +

--- my_config/config_log_yechan/roi_transformer/amod/roi_transformer_swinS_fpn_angle0,10,20,30,40,50_30epochs_le90_amod.py
+++ my_config/config_log_yechan/roi_transformer/amod/roi_transformer_swinS_fpn_angle0,10,20,30,40,50_30epochs_le90_amod.py
@@ -104,7 +104,6 @@ model = dict(
         in_channels=[96, 192, 384, 768],
         # in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        start_level=1,
         add_extra_convs='on_input',
         num_outs=5),
     rpn_head=dict(
