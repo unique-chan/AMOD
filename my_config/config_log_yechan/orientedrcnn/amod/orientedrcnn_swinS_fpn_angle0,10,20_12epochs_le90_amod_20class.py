e2ac4200d707c3c523c9fcb1c2b0de7f46ab78a8
unique-chan <yechankim@gm.gist.ac.kr>
2025-04-05 18:06:36 +0900

    +

--- my_config/config_log_yechan/orientedrcnn/amod/orientedrcnn_swinS_fpn_angle0,10,20_12epochs_le90_amod_20class.py
+++ my_config/config_log_yechan/orientedrcnn/amod/orientedrcnn_swinS_fpn_angle0,10,20_12epochs_le90_amod_20class.py
@@ -1,6 +1,6 @@
 # 🚩 DEFAULT CONFIG ####################################################################################################
 dataset_type = 'AMODDataset'
-angles = [0, 10, 20, 30, 40, 50]
+angles = [0, 10, 20]
 data_root = 'data/AMOD_V1/'         # Important: should be ended with '/'
 modality = 'EO'                     # 'eo' or 'ir'
 img_extension = 'png'               # 'png' or 'jpg'
