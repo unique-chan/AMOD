650b62fff53ce8902c1119ae0e5786d3951be801
unique-chan <yechankim@gm.gist.ac.kr>
2025-05-04 02:00:08 +0900

    +

--- mmrotate/mmrotate/datasets/__init__.py
+++ mmrotate/mmrotate/datasets/__init__.py
@@ -7,5 +7,5 @@ from .sar import SARDataset  # noqa: F401, F403
 from .amod import *  # noqa: F401, F403
 
 __all__ = ['SARDataset', 'DOTADataset', 'build_dataset', 'HRSCDataset',
-           'AMODDataset', 'AMODFineGrainedDataset',
-           'AMODwithCivilianDataset', 'AMODwithCivilianFineGrainedDataset']
+           'AMODDataset'] #, 'AMODFineGrainedDataset',
+           # 'AMODwithCivilianDataset', 'AMODwithCivilianFineGrainedDataset']
