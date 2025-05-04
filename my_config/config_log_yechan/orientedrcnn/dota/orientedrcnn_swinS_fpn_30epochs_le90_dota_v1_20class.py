c955112978a8ed1e1a4fab17a8f93343da7d0a0a
unique-chan <yechankim@gm.gist.ac.kr>
2025-05-04 14:47:28 +0900

    +

--- my_config/config_log_yechan/orientedrcnn/dota/orientedrcnn_swinS_fpn_30epochs_le90_dota_v1_20class.py
+++ my_config/config_log_yechan/orientedrcnn/dota/orientedrcnn_swinS_fpn_30epochs_le90_dota_v1_20class.py
@@ -58,8 +58,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    # samples_per_gpu=2,
+    # workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'train/annfiles/',
